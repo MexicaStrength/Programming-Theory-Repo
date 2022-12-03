25cc4dd73c3374552bca60ce08a9a30329d63e99
TSS-Lago <bravo.opus@outlook.com>
2022-12-03 16:17:39 -0500

    Gravity + Jumping
    added gravity and jumping to player (ty brackeys once again)

--- Yippee Simulator 3D/Assets/Scripts/PlayerMovement.cs
+++ Yippee Simulator 3D/Assets/Scripts/PlayerMovement.cs
@@ -10,14 +10,31 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 10f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+    public float jumpHeight = 5f;
     public Transform cam;
+    public Vector3 velocity;
+    public float grav = -4.9f;
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+    public bool onGround;
     // Update is called once per frame
     void Update()
     {
+        //constantly checks if player is on the ground
+        onGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(onGround && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //directional input from player
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        //determines player/camera rotation based on movement
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -26,5 +43,15 @@ public class PlayerMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+        //jumping
+        if(Input.GetButtonDown("Jump") && onGround)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * grav);
+        }
+
+        //adding gravity to the player
+        velocity.y += grav * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
